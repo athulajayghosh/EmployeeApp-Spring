c604125054bb1cb32a298a0245bb7401c408f09d
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-12-05 15:33:53 +0530

    pages linked commit

--- src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employeeapp_backend/controller/EmployeeController.java
@@ -1,2 +1,36 @@
-package com.example.employeeapp_backend.controller;public class EmployeeController {
+package com.example.employeeapp_backend.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+    @GetMapping("/")
+    public String Homepage(){
+        return "Homepage";
+    }
+
+    @GetMapping("/addemp")
+    public String addemp(){
+        return "Add Employee Page";
+    }
+
+    @GetMapping("/searchemp")
+    public String searchemp(){
+        return "Search Employee page";
+    }
+    @GetMapping("/editemp")
+    public String editemp(){
+        return "Edit employee page";
+    }
+    @GetMapping("/viewallemp")
+    public String viewallemp(){
+        return "View all employee page";
+    }
+    @GetMapping("/deleteemp")
+    public String deleteemp(){
+        return "delete employee page";
+    }
+
 }
