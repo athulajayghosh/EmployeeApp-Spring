8436fc6b9150e223cafd4f10787bc5268991bb51
athulajayghosh <53214137+athulajayghosh@users.noreply.github.com>
2022-12-06 15:04:32 +0530

    connected to sql

---
+++ src/main/java/com/example/employeeapp_backend/dao/CourseDao.java
@@ -0,0 +1,8 @@
+package com.example.employeeapp_backend.dao;
+
+import com.example.employeeapp_backend.model.Employee;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseDao extends CrudRepository<Employee,Integer> {
+
+}
